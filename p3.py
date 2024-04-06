d7bbcc0ed0bdc2cb1660c0b4dc7d430837661384
Ahmed_Elsayed <ahmed.dev2013@gmail.com>
2024-04-06 22:25:22 +0200

    changing the function to return the the letter instead of printing

--- p3.py
+++ p3.py
@@ -4,8 +4,8 @@ class Student:
         self.age=age
         self.grade=grade
     def get_letter_grade(self):
-        print('A' if self.grade >=90 else 'B' if self.grade >=80 else 'C' if self.grade>70 else 'D')
+        return 'A' if self.grade >=90 else 'B' if self.grade >=80 else 'C' if self.grade>70 else 'D'
 
 
 s=Student('Ahmed',22,93)
-s.get_letter_grade()
+print(f"The grade of {s.name} is {s.get_letter_grade()}")
