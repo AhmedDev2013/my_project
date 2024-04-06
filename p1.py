0712a3716cbb7cfeb0747496a7f2e61faf474cee
Ahmed_Elsayed <ahmed.dev2013@gmail.com>
2024-04-06 16:59:36 +0200

    using list comprehension in problem1

--- p1.py
+++ p1.py
@@ -1,11 +1,8 @@
 x,y=int(input("Enter x: ")),int(input("Enter y: "))
 if x>y:
     x,y=y,x
-res=0
-res_str=""
-for i in range(x,y+1):
-    if i%2==0:
-        res+=i
-        res_str+=str(i)+"+"
 
-print(f"{res_str[:-1]} = {res}")
+numbers=[i for i in range(x,y+1) if i%2==2]
+res=sum(numbers)
+res_str="+".join([str(a) for a in numbers])
+print(f"{res_str} = {res}")
